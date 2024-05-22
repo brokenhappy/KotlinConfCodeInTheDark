db71de708b0608b4acaab04b14f07eaba0ca3421
wout.werkman <wout.werkman@jetbrains.com>
2024-05-22 17:13:12 +0200

    Remove do not touch naming

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -12,41 +12,44 @@ import androidx.compose.ui.text.font.*
 import androidx.compose.ui.unit.*
 import codeinthedark.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 const val teamName = "Unnamed contestant!"
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalLayoutApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        Column {
-            Box(Modifier.fillMaxHeight(.5f)) {
-                Image(painterResource(Res.drawable.kodee_waving), null)
-            }
-            Text(
-                text = "Hello",
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
-            )
-            Text(
-                text = " World!",
-                color = Color(red = 107, green = 48, blue = 255),
-//                color = Color(0xFF7954F6), // Alternative using ARGB
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-            )
-            TextField(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                value = "",
-                onValueChange = {},
-                placeholder = {
-                    Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "Insert text here")
-                },
-            )
-            Button(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = {},
-            ) {
-                Text("Don't click me!")
+        FlowRow {
+            Column {
+                Box(Modifier.fillMaxHeight(.5f)) {
+                    Image(painterResource(Res.drawable.kodee_waving), null)
+                }
+                Text(
+                    text = "Hello",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                )
+                Text(
+                    text = " World!",
+                    color = Color(red = 107, green = 48, blue = 255),
+    //                color = Color(0xFF7954F6), // Alternative using ARGB
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                )
+                TextField(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "Insert text here")
+                    },
+                )
+                Button(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = {},
+                ) {
+                    Text("Don't click me!")
+                }
             }
         }
     }
